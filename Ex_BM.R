bb3ff8acb5bf6f62e264deb8eb2f07f98f2a90d1
unknown <chiunshiun@gmail.com>
2019-12-13 06:37:32 +0800

    Final Commit

--- Ex_BM.R
+++ Ex_BM.R
@@ -1,19 +1,22 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-import::here(sSINDy,sSINDy_new,order_coef,error_compute,gene_poly_data,
+import::here(sSINDy,order_coef,error_compute,gene_poly_data,
              .from = 'sSINDy.R')
 
 
 # Try to fit Brownian motion which start at x0=10
 end_time=1 #The end time of the Brownian motion
-n_times=1000 # How many time step b/t [0,end_time]
+n_times=100000 # How many time step b/t [0,end_time]
 
 X=sde::BM(x=10, t0=0, T=end_time, N=n_times) # Generate BM
 t_diff=end_time/n_times # Length of time interval
-y_data=(X[2:(n_times+1)]-X[1:n_times])/t_diff # Change of BM b/t time
+y_data=(X[2:(n_times+1)]-X[1:n_times]) # Change of BM b/t time
 X_data=X[1:n_times] 
 
+#The relation b/t X and dX
+plot(X_data,y_data)
+
 # Some facts for y_data
-plot(density(y_data)) #This plot should be similar to normal N(0,var=t)
+plot(density(y_data)) #This plot should be similar to normal N(0,var=dt)
 mean(y_data)
 var(y_data)
 
@@ -23,51 +26,42 @@ n_fold=10 # folds of CV
 
 # Fit drift part
 # The real model is b(X)=0
-sSINDy_new(gene_poly_data(X_data,3),y_data,10)
+sSINDy(gene_poly_data(X_data,3),y_data,10)
 
-#Fit diffsion term
-variation=(X[2:(n_times+1)]-X[1:n_times])*(X[2:(n_times+1)]-X[1:n_times])/t_diff
-mean(variation)
-plot(variation)
-sSINDy_new(gene_poly_data(X_data,3),variation,10)
-
+#Compute the diffsion term
+sd(y_data/sqrt(t_diff))
 
 
 
 #############################################################
+# Drift Term
 # Simuation to check if we can reconstruct the drift term
 simulate_BM_drift=function(fun_sim,end_T,n_times,k_fold,order_poly=3){
   t_diff=end_T/n_times
   X=fun_sim(x=10, t0=0, end_T, n_times)
-  y_data=(X[2:(n_times+1)]-X[1:n_times])
+  y_data=(X[2:(n_times+1)]-X[1:n_times])/t_diff
   X_data=X[1:n_times]
-  return(sSINDy_new(gene_poly_data(X_data,order_poly),
+  return(sSINDy(gene_poly_data(X_data,order_poly),
                     y_data,k_fold)$coef)
 }
 
-coef_sim=replicate(1000,simulate_BM_drift(sde::BM,1,100,10))
+# Replicate some times and compute the CI for Drift Term
+coef_sim=replicate(1000,simulate_BM_drift(sde::BM,1,1000,10))
 apply(coef_sim,1,quantile,prob=c(0.025,0.975))
 sum(coef_sim!=0) # really few components is zero
 
-
+# Diffusion Term
 # Simuation to check if we can reconstruct the diffusion term
-simulate_BM_diffusion=function(fun_sim,end_T,n_times,k_fold,order_poly=3){
+simulate_BM_diffusion=function(fun_sim,end_T,n_times){
   t_diff=end_T/n_times
   X=fun_sim(x=10, t0=0, end_T, n_times)
-  variation=(X[2:(n_times+1)]-X[1:n_times])*(X[2:(n_times+1)]-X[1:n_times])/t_diff
-  X_data=X[1:n_times]
-  return(sSINDy_new(gene_poly_data(X_data,order_poly),
-                    variation,k_fold)$coef)
+  y_data=(X[2:(n_times+1)]-X[1:n_times])
+  return("Diffusion"=sd(y_data/sqrt(t_diff)))
 }
 
-coef_sim=replicate(1000,simulate_BM_diffusion(sde::BM,1,1000,10))
-apply(coef_sim,1,summary)
+# Replicate some times and compute the CI for Diffusion Term
+coef_sim=replicate(100,simulate_BM_diffusion(sde::BM,1,10000))
+quantile(coef_sim,prob=c(0.025,0.975))
 
-#The numbers of bad result
-sum(abs(coef_sim[1,]-1)>1e-1)
-sum(abs(coef_sim[2,])>1e-2)
-
-plot(abs(coef_sim[1,]-1))
-plot(abs(coef_sim[2,]))
 
 
--- Ex_GBM.R
+++ Ex_GBM.R
@@ -1,21 +1,22 @@
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-import::here(sSINDy,sSINDy_new,order_coef,error_compute,gene_poly_data,
+import::here(sSINDy,order_coef,error_compute,gene_poly_data,
              .from = 'sSINDy.R')
 
 
 
 # Try to fit Geometric-Brownian motion which start at x0=1
 # dX = rX dt + sigma X dB
-end_time=10 #The end time of the Brownian motion
-n_times=1000 # How many time step b/t [0,end_time]
+end_time=1 #The end time of the Brownian motion
+n_times=10000 # How many time step b/t [0,end_time]
 
-X=sde::GBM(x=1, r=1,sigma=1, T=end_time, N=n_times) # Generate BM
+X=sde::GBM(x=1, r=1,sigma=.1, T=end_time, N=n_times) # Generate BM
 t_diff=end_time/n_times # Length of time interval
 y_data=(X[2:(n_times+1)]-X[1:n_times])/t_diff # Change of GBM b/t time
 X_data=X[1:n_times] 
 
 # Some facts for y_data
 plot(X_data,y_data) #y has bigger variance as X becomes bigger
+plot(X_data,type="l")
 
 
 #############################################################
@@ -24,7 +25,9 @@ n_fold=10 # folds of CV
 
 # Fit drift part
 # The real model is b(X)=X
-sSINDy_new(gene_poly_data(X_data,3),y_data,10)
+sSINDy(gene_poly_data(X_data,3),y_data,10)
+
+lm(y_data~X_data-1)%>%coef
 
 #Fit diffsion term
 variation=(X[2:(n_times+1)]-X[1:n_times])*(X[2:(n_times+1)]-X[1:n_times])/t_diff
@@ -32,8 +35,16 @@ mean(variation)
 plot(variation)
 sSINDy_new(gene_poly_data(X_data,3),variation,10)
 
+#By lasso
+cv_output <- glmnet::cv.glmnet(gene_poly_data(X_data,3), 
+                            y_data,intercept=FALSE)
+coef(cv_output,cv_output$lambda.min)
 
-
+#By relaxed lasso
+cv_relaxo=relaxo::cvrelaxo(gene_poly_data(X_data,3), 
+               y_data,K=5)
+cv_relaxo$beta
+relaxo::plot.relaxo(cv_relaxo)
 
 #############################################################
 # Simuation to check if we can reconstruct the drift term
@@ -43,7 +54,7 @@ simulate_GBM_drift=function(fun_sim,r=1,sigma=1,end_T,
   X=fun_sim(x=1, r,sigma, T=end_time, N=n_times)
   y_data=(X[2:(n_times+1)]-X[1:n_times])/t_diff
   X_data=X[1:n_times]
-  return(sSINDy_new(gene_poly_data(X_data,order_poly),
+  return(sSINDy(gene_poly_data(X_data,order_poly),
                     y_data,k_fold)$coef)
 }
 
@@ -59,7 +70,7 @@ simulate_GBM_diffusion=function(fun_sim,r=1,sigma=1,end_T,
   X=fun_sim(x=1, r,sigma, T=end_time, N=n_times)
   variation=(X[2:(n_times+1)]-X[1:n_times])*(X[2:(n_times+1)]-X[1:n_times])/t_diff
   X_data=X[1:n_times]
-  return(sSINDy_new(gene_poly_data(X_data,order_poly),
+  return(sSINDy(gene_poly_data(X_data,order_poly),
                     variation,k_fold)$coef)
 }
 
